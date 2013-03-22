8a90f03262fab0fb6e422c4975cd8fab2bc64afe
blithexu <blithexu@gmail.com>
2013-03-22 20:54:43 +0800

    update example code

---
+++ Sources/Client/iOS/CacheControlExample/CacheControlAppConstants.h
@@ -0,0 +1,14 @@
+//
+//  CacheControlAppConstants.h
+//  CacheControlExample
+//
+//  Created by UX on 3/22/13.
+//  Copyright (c) 2013 Shaoke Xu. All rights reserved.
+//
+
+#ifndef CacheControlExample_CacheControlAppConstants_h
+#define CacheControlExample_CacheControlAppConstants_h
+
+
+
+#endif
